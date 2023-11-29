d044e56b83ba150570460b213e3e702104d43895
mohammadkhawari <152442137+mohammadkhawari@users.noreply.github.com>
2023-11-30 02:38:02 +0330

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,12 @@
+a=input("enter a(+, -, *, /):")
+b=int(input("enter b:"))
+c=int(input("enter c:"))
+if a== "+":
+    print(b+c)
+if a== "-":
+    print(b-c)
+if a=="*":
+    print(b*c)
+if a=="/":
+    print(b/c)            
+        
