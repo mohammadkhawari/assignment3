3fc734c8e1f0a4698956cbb01f279b8247bc96fd
mohammadkhawari <152442137+mohammadkhawari@users.noreply.github.com>
2023-11-30 02:24:19 +0330

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,7 @@
+a=int(input("enter number:"))
+b=int(input("enter number:"))
+g=int(input("enter number:"))
+if a+b>g and a+g>b and g+b>a:
+    print("mission done")
+else:
+    print("failed to draw!")    
